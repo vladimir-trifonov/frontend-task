7bbcda0df49338619766eca88a49bd0aef2ed604
ayoola-solomon <ayoola.solomon89@gmail.com>
2019-02-07 15:12:27 +0100

    initial setup

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const rootElem = document.getElementById('root');
+
+const html = rootElem.innerHTML;
+rootElem.innerHTML = 'Welcome to the Adeva take home test. Please follow the instructions in the README and have fun 😃.';
